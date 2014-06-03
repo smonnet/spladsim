4ba7d8deea00ad84ea53a77d0508ab8b770a1c72
Sebastien Monnet <Sebastien.Monnet@lip6.fr>
2014-06-03 22:49:23 +0200

    adding simgrid squeleton

---
+++ SpladSimJava/src/spladsim/BasicTask.java
@@ -0,0 +1,5 @@
+package test;
+public class BasicTask extends org.simgrid.msg.Task {
+public BasicTask(String name, double computeDuration, double messageSize) {
+super(name, computeDuration, messageSize);
+} }
---
+++ SpladSimJava/src/spladsim/FinalizeTask.java
@@ -0,0 +1,6 @@
+package test;
+public class FinalizeTask extends org.simgrid.msg.Task {
+public FinalizeTask() {
+super("finalize",0,0);
+} 
+}
