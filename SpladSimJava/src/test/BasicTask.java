4ae7ca6aaa21a5413648ce03c9d7dac3fc445b10
Sebastien Monnet <Sebastien.Monnet@lip6.fr>
2014-06-04 10:41:47 +0200

    renaming

---
+++ SpladSimJava/src/test/BasicTask.java
@@ -0,0 +1,6 @@
+package test;
+public class BasicTask extends org.simgrid.msg.Task {
+	public BasicTask(String name, double computeDuration, double messageSize) {
+		super(name, computeDuration, messageSize);
+	}
+}
