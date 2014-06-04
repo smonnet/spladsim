d200b0898299f4161bcc89eae6c5b81053010a95
Sebastien Monnet <Sebastien.Monnet@lip6.fr>
2014-06-04 10:42:53 +0200

    moving

--- SpladSimJava/src/spladsim/BasicTask.java
+++
@@ -1,6 +0,0 @@
-package spladsim;
-public class BasicTask extends org.simgrid.msg.Task {
-	public BasicTask(String name, double computeDuration, double messageSize) {
-		super(name, computeDuration, messageSize);
-	}
-}
--- SpladSimJava/src/spladsim/FinalizeTask.java
+++
@@ -1,6 +0,0 @@
-package spladsim;
-public class FinalizeTask extends org.simgrid.msg.Task {
-public FinalizeTask() {
-super("finalize",0,0);
-} 
-}
