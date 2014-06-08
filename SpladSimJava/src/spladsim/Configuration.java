d95c257cf3471a726d6438b2ceac6ef2e7f04095
Sebastien Monnet <Sebastien.Monnet@lip6.fr>
2014-06-08 16:18:24 +0200

    Done with the configuration engine

--- SpladSimJava/src/spladsim/Configuration.java
+++ SpladSimJava/src/spladsim/Configuration.java
@@ -23,7 +23,7 @@ public class Configuration {
 	
 	int fileSize;
 	long nbFiles;
-	int rf;
+	int replFactor;
 	
 	double endTime;
 	int seed;
@@ -36,14 +36,42 @@ public class Configuration {
 		String buf = null;
 		try {
 			properties.load(new FileInputStream(configFile));
+			
+			buf = properties.getProperty("maintenancePeriod");
+			maintenancePeriod = Double.parseDouble(buf);
+			Msg.info("maintenancePeriod::"+maintenancePeriod);
+			
+			buf = properties.getProperty("selectionRange");
+			selectionRange = Long.parseLong(buf);
+			Msg.info("selectionRange::"+selectionRange);
+
+			buf = properties.getProperty("placementPolicie");
+			placementPolicie = Integer.parseInt(buf);
+			Msg.info("placementPolicie::"+placementPolicie);
+			
+			buf = properties.getProperty("fileSize");
+			fileSize = Integer.parseInt(buf);
+			Msg.info("fileSize::"+fileSize);
+			
+			buf = properties.getProperty("nbFiles");
+			nbFiles = Long.parseLong(buf);
+			Msg.info("nbFiles::"+nbFiles);
+			
+			buf = properties.getProperty("replFactor");
+			replFactor = Integer.parseInt(buf);
+			Msg.info("replFactor::"+replFactor);
+			
+			buf = properties.getProperty("endTime");
+			endTime = Double.parseDouble(buf);
+			Msg.info("endTime::"+endTime);
+			
 			buf = properties.getProperty("seed");
+			seed = Integer.parseInt(buf);
+			Msg.info("seed::"+seed);
+
 		} catch (IOException e) {
 			Msg.info("Error reading configuration file : " + configFile);
 			e.printStackTrace();
-		}
-		// convertion to long
-		try {
-			seed = Integer.parseInt(buf);
 		} catch (NumberFormatException n) {
 			Msg.info("Error while trying to convert to seeg to int : " + buf);
 		}
